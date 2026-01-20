dc89dfcc10848fa3587665e53bee254b58136ee1
MatthieuGillieron <gillieron.matthieu@ik.me>
2026-01-20 13:45:01 +0100

    init: Ex01

---
+++ ex01/Serializer.hpp
@@ -0,0 +1,19 @@
+
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include <string>
+#include <iostream>
+
+class Seriallizer
+{
+
+	private:
+
+
+	public:
+
+};
+
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,15 @@
+
+#include "Serializer.hpp"
+
+int main(int ac, char **av)
+{
+
+	if (ac != 2)
+	{
+		std::cout << "try: ./exec00 <your_test>" << std::endl;
+		return 1;
+	}
+
+	
+	return 0;
+}
